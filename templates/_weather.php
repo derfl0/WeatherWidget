b16fa8ad1f84ce89306c88e5b141e0cc59f8194c
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-11-27 11:36:54 +0100

    Open icons via ssl to make the site secure

--- templates/_weather.php
+++ templates/_weather.php
@@ -1,6 +1,6 @@
 <div class="weather">
     <h3><?= htmlReady($title) ?></h3>
-    <img src="http://openweathermap.org/img/w/<?= htmlReady($weather->weather[0]->icon) ?>.png">
+    <img src="https://openweathermap.org/img/w/<?= htmlReady($weather->weather[0]->icon) ?>.png">
     <p><?= round(($weather->main ? $weather->main->temp : $weather->temp->day) - 273.15) ?>°</p>
     <p><?= htmlReady(_($weather->weather[0]->description)) ?></p>
 </div>
